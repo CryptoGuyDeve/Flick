31071dbf6baa37bb34ea8b6ed868046f3467d12e
CryptoGuyDeve <m.faizurrehman.crypto@gmail.com>
2025-05-28 00:14:29 +0500

    Updated and going to build it

--- app.config.js
+++ app.config.js
@@ -1,37 +1,37 @@
-module.exports = {
-  expo: {
-    name: "Flick",
-    slug: "flick",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "dark",
-    assetBundlePatterns: [
-      "**/*"
-    ],
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.flick.app"
+export default {
+  name: "Flick",
+  slug: "flick",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "dark",
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#000000"
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.flick.app"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#000000"
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#000000"
-      },
-      package: "com.flick.app"
-    },
-    web: {
-      favicon: "./assets/favicon.png"
-    },
-    plugins: [
-      "expo-router",
-      "expo-notifications"
-    ],
-    scheme: "flick",
-    extra: {
-      eas: {
-        projectId: "flick-app-1234-5678-9012-3456"
-      }
+    package: "com.flick.app",
+    versionCode: 1,
+    edgeToEdgeEnabled: true
+  },
+  web: {
+    favicon: "./assets/favicon.png"
+  },
+  extra: {
+    eas: {
+      projectId: "6af8fd56-fcd7-44aa-939e-1be0a7d8da58"
     }
   }
 }; 
